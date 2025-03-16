601221400ab33dfcdd5cd8e7cd56dbd02ce26fbf
AdielHidalgo <143145253+AdielHidalgo@users.noreply.github.com>
2025-03-16 16:18:26 -0600

    operaciones basicas

---
+++ P002_OperacionesAritmeticas.py
@@ -0,0 +1,8 @@
+#En este programa se ven los operadores aritmeticos
+
+suma= 239+56
+resta=567-466
+mutiplicacion=55*46
+division=789/67
+
+print(" 239+56=",suma,"\n","567-466=",resta,"\n","55*46=",mutiplicacion,"\n","789/67=",division)
