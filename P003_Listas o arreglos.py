e77f31b86f71693fcf2036e6c282553f8dc95524
AdielHidalgo <143145253+AdielHidalgo@users.noreply.github.com>
2025-03-16 16:39:27 -0600

    se crea una lista de automoviles

---
+++ P003_Listas o arreglos.py
@@ -0,0 +1,7 @@
+#En este programa se va a trabajar con listas
+#Un lista es una coleccion de elementos, una variable con multiples valores
+
+Automoviles = ["Nissan",'WW','BMW','Tesla']
+print(Automoviles)
+#Para imprimir un solo elemento de la lista se especifica la posicion del elemento
+print("\n",Automoviles[3])
