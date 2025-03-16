179243060371fc57ebc3707481139c3d5b3a988c
AdielHidalgo <143145253+AdielHidalgo@users.noreply.github.com>
2025-03-16 16:49:48 -0600

    posiciones negativas

--- P003_Listas o arreglos.py
+++ P003_Listas o arreglos.py
@@ -1,7 +1,16 @@
-#En este programa se va a trabajar con listas
+####En este programa se va a trabajar con listas####
+#~~~~  1.- Introducción a las listas ~~~~
 #Un lista es una coleccion de elementos, una variable con multiples valores
 
 Automoviles = ["Nissan",'WW','BMW','Tesla']
 print(Automoviles)
 #Para imprimir un solo elemento de la lista se especifica la posicion del elemento
-print("\n",Automoviles[3])
+print("\n",Automoviles[3],"\n\n\n")
+
+#~~~ 2.- Posiciones negativas ~~~
+#En una lista las posiciones negativas son las posiciones en las cuales se empiza desde el
+#ultimo elemento de la lista hacia el primero.
+
+Alumnos = ['Lupe','Ramon','Jona','Raul','Lucrecio','Armando','Josefina']
+
+print("El último alumno de la lista es: ",Alumnos[-1])
