58217e1c064d9e87af28f455704374756f25ff1c
AdielHidalgo <143145253+AdielHidalgo@users.noreply.github.com>
2025-03-16 16:29:47 -0600

    Se agregan numero de tipo float

--- P002_OperacionesAritmeticas.py
+++ P002_OperacionesAritmeticas.py
@@ -1,5 +1,6 @@
 #En este programa se ven los operadores aritmeticos
 
+#Para suma, resta, mutiplicacion y division
 suma= 239+56
 resta=567-466
 mutiplicacion=55*46
@@ -7,5 +8,15 @@ division=789/67
 
 print(" 239+56=",suma,"\n","567-466=",resta,"\n","55*46=",mutiplicacion,"\n","789/67=",division)
 
+#Para un numero x elevado a una potencia y
 Elevaciones=54**7
 print('\nLa potencia del numero 54 elevado a la 7 es=',Elevaciones)
+
+#Para numero flotantes o decimales
+N1=256.678
+N2=467.55543
+Resultado=N1+N2
+print("Resultado sin redondeo ",Resultado)
+#Si queremos solo cierto numero de decimales se utiliza el siguiete formato
+#print(round(variable,numeroDeDecimales))
+print("Resultado con redondeo ",round(Resultado,3))
