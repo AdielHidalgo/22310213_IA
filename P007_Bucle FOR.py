08775e5e657a2a394f6c008850f51e4ff10fc770
AdielHidalgo <143145253+AdielHidalgo@users.noreply.github.com>
2025-03-16 22:02:32 -0600

    Utilidades del for

--- P007_Bucle FOR.py
+++ P007_Bucle FOR.py
@@ -1,6 +1,6 @@
 #BUCLE "FOR"
-
-# 1.- 
+"""
+# 1.- Introduccion al for
 
 #for x in "Phyton":
 #    print(x)
@@ -20,3 +20,35 @@ for x in Materias:
     if x == "Hidraulica":
         break
     print(x)
+
+# 2.- La funcion range() en el for
+
+#Se le coloca un rango con inicio en 0 y incremento en 1
+#for x in range(10):
+ #   print(x)
+
+#Se coloca un rango de 5-10 con incremento en 1
+#for x in range(5,10):
+ #   print(x)
+
+#Se coloca un rango de 5 a 25 con incremento en 5
+#for x in range(5,25,5):
+ #   print(x)
+
+#sacar los valores de una lista y que realice operaciones
+
+print("Lista de números:\n")
+numeros=[2,4,8,16,32,64,128]
+for x in range(len(numeros)):
+    print("Número de operación -> ",x, "Multiplicacion ->", numeros[x],"Resultados: ->", numeros[x] * numeros[x])
+"""
+
+#For anidado
+
+num1 = ["1","2","3","4","5"]
+num2 = "0"
+num3= "A"
+for x in num1:
+    for y in num2:
+        for z in num3:
+            print(x + y + z)
