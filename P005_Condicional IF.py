e184c6f90cd766056307fa72b0cc01352f2860f9
AdielHidalgo <143145253+AdielHidalgo@users.noreply.github.com>
2025-03-16 19:39:53 -0600

    condicion elif

--- P005_Condicional IF.py
+++ P005_Condicional IF.py
@@ -5,6 +5,8 @@
 # <= menor o igual que
 # == igual que
 # != diferente que
+# and operacion logica y
+# or operacion logica ó 
 """
 num1 = 1450
 num2 = 60
@@ -15,8 +17,8 @@ if num1 == num2:
 	print('Se ejecuta el if.')
 if num1  num2:
 	print('Se ejecuta el if.')
-"""
 
+#condicion if- else
 print("Toñito quiere comprar unos tenis de $250")
 Toñito=250
 print("Toñito tiene: $",Toñito)
@@ -24,3 +26,20 @@ if Toñito >=250:
     print("Toñito puede comprar los tenis, trae con queso")
 else:
     print("Toñito no puede comprar los tenis,anda pobre")
+"""
+#condicion elif y entrada de datos
+print("Estas en una tienda y estas pensando comprar unas papitas $20, una coca $25, unos chicles $2 y las tortillas $24")
+Dinero = int(input("Dime con cuanta feria cuentas:  "))
+if Dinero >= 20 and Dinero<25:
+    print("Puedes comprar unas papitas")
+elif Dinero >= 2 and Dinero<20:
+    print("Puedes comprar unos chicles")
+elif Dinero >= 24 and Dinero<=25:
+    print("Puedes comprar una coca o las tortillas")
+elif Dinero >= 71:
+    print("Genial traes billete pa llevarte lo que estas pensando")
+else:
+    print("Sorry no te ajusta, regresa cuando traigas feria suficiente")
+
+
+
