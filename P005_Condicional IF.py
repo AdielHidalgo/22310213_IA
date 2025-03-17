4d374854b338891b7fdafd11698205dd128e94ce
AdielHidalgo <143145253+AdielHidalgo@users.noreply.github.com>
2025-03-16 19:12:37 -0600

    condicion if

---
+++ P005_Condicional IF.py
@@ -0,0 +1,13 @@
+#CONDICIONAL "IF"
+# > mayor que
+# < menor que
+# >= mayor o igual que
+# <= menor o igual que
+# == igual que
+# != diferente que
+
+num1 = 1450
+num2 = 60
+
+if num1 != num2:
+	print('Se ejecuta el if.')
