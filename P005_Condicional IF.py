de45666b283eb68f7bd83c83edac6727dd24c509
AdielHidalgo <143145253+AdielHidalgo@users.noreply.github.com>
2025-03-16 19:19:45 -0600

    if else

--- P005_Condicional IF.py
+++ P005_Condicional IF.py
@@ -5,9 +5,22 @@
 # <= menor o igual que
 # == igual que
 # != diferente que
-
+"""
 num1 = 1450
 num2 = 60
 
 if num1 != num2:
 	print('Se ejecuta el if.')
+if num1 == num2:
+	print('Se ejecuta el if.')
+if num1  num2:
+	print('Se ejecuta el if.')
+"""
+
+print("Toñito quiere comprar unos tenis de $250")
+Toñito=250
+print("Toñito tiene: $",Toñito)
+if Toñito >=250:
+    print("Toñito puede comprar los tenis, trae con queso")
+else:
+    print("Toñito no puede comprar los tenis,anda pobre")
