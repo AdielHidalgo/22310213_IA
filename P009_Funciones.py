ac96b94169e86275189c867afc54d11e8bd6f179
AdielHidalgo <143145253+AdielHidalgo@users.noreply.github.com>
2025-03-17 20:47:25 -0600

    introduccion a las funciones

---
+++ P009_Funciones.py
@@ -0,0 +1,17 @@
+###Funciones###
+#Es un bloque de codigo que solo se ejcuta cuando lo llamemos
+
+def saluda():
+    print("Hola, En este programa vamos a aprender a realizar funciones")
+saluda()
+
+print("La familia Simpson\n")
+
+def familiaS(nombre,parentesco):
+    print(nombre + " Simpson-" + parentesco)
+
+familiaS("Homer","Padre")
+familiaS("Marge","Madre")
+familiaS("Lisa","Hija")
+familiaS("Bart","Hijo")
+familiaS("Maggie","Hija")
